4bf8f5cd9562541b3d80ad3c39ab0a715a1435a4
Shahar Schneor <schneor13@gmail.com>
2017-10-07 03:29:21 -0400

    src: deploy script

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+git clone https://06ce60089f45222b0c5e3412304466cf97be64e2:x-oauth-basic@github.com/schneor13/nodejs-dhcp-server.git /schneor13/nodejs-dhcp-server --branch develop
