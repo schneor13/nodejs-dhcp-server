be57098accf0029de77cf9f41d383395563f9bbe
Shahar Schneor <schneor13@gmail.com>
2017-10-07 04:28:27 -0400

    src: update server.js ips config

--- app/server.js
+++ app/server.js
@@ -10,19 +10,19 @@ console.log(`-- Starting Server.... ---`);
 const dhcp = require("dhcp");
 const server = dhcp.createServer({
     range: [
-        "192.168.3.10", "192.168.3.99"
+        "172.30.0.10", "172.30.255.255"
     ],
     forceOptions: ['hostname'], // Options that need to be sent, even if they were not requested
-    static: {
+    /*static: {
         "11:22:33:44:55:66": "192.168.3.100"
-    },
+    },*/
     // Option settings
-    netmask: '255.255.255.0',
+    netmask: '255.255.0.0',
     router: [
-        '192.168.0.1'
+        '172.30.0.1'
     ],
     dns: ["8.8.8.8", "8.8.4.4"],
-    server: '192.168.0.1', // This is us
+    server: '172.30.0.1', // This is us
     hostname: function () {
         const hostname = `schneorhost${i++}`;
         console.log(`-- Hostname: ${hostname} .... ---`);
