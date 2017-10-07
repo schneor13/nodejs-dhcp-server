67041b55854e2fabc0f176b42f55cc91106f342e
Shahar Schneor <schneor13@gmail.com>
2017-10-07 04:01:44 -0400

    src: update server config

--- server.js
+++ server.js
@@ -9,29 +9,22 @@ console.log('-- Starting Server.... ---');
 
 const dhcp = require("dhcp");
 const dhcpServer = dhcp.createServer({
-    // System settings
     range: [
-        "192.168.3.10",
-        "192.168.3.99"
+        "192.168.3.10", "192.168.3.99"
     ],
+    forceOptions: ['hostname'], // Options that need to be sent, even if they were not requested
     static: {
         "11:22:33:44:55:66": "192.168.3.100"
     },
-
-    // Option settings (there are MUCH more)
+    // Option settings
     netmask: '255.255.255.0',
     router: [
         '192.168.0.1'
     ],
-    bootFile: function (req) {
-
-        console.log('-- Booting.... ---');
-
-        if (req.clientId === 'foo bar') {
-            return 'x86linux.0';
-        } else {
-            return 'x64linux.0';
-        }
+    dns: ["8.8.8.8", "8.8.4.4"],
+    server: '192.168.0.1', // This is us
+    hostname: function () {
+        return 'schenorhost' + i++;
     }
 });
 
