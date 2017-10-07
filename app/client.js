65a65f67fc457f6310b967259e7883fe351f3d04
Shahar Schneor <schneor13@gmail.com>
2017-10-08 02:48:18 +0300

    src: rewriting client side
    creating a backup file for client side and erasing all current client side code.

--- app/client.js
+++ app/client.js
@@ -4,22 +4,3 @@
  * author: schneor13
  * ---------------------------------
  */
-
-
-let dhcp = require('dhcp');
-let client = dhcp.createClient();
-
-client.on('bound', function () {
-
-    console.log("State: ", this._state);
-
-    // `ip address add IP/MASK dev eth0`
-    // `echo HOSTNAME > /etc/hostname && hostname HOSTNAME`
-    // `ip route add default via 192.168.1.254`
-    // `sysctl -w net.inet.ip.forwarding=1`
-
-});
-
-client.listen();
-
-client.sendDiscover();
